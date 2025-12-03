a6d6915b8d7d917888ccbcd468288219c0cd5553
Nick Allen <nick.allen@dataglide.co>
2025-12-03 00:20:01 +0000

    Complete day1 part 2

--- 2025/day1/src/main.rs
+++ 2025/day1/src/main.rs
@@ -1,21 +1,48 @@
 use std::fs;
 
 fn main() {
-    let part1 = fs::read_to_string("part-1.txt").expect("couldn't find part-1.txt");
-    let instructions = part1.split_whitespace().map(|item| Instruction::new(item));
+    part2();
+}
+
+fn part1() {
+    // let input = fs::read_to_string("input.txt").expect("couldn't find part-1.txt");
+    let input = fs::read_to_string("test.txt").expect("couldn't find part-1.txt");
+    let instructions = input.split_whitespace().map(|item| Instruction::new(item));
 
     let mut count_of_zeros = 0;
     let mut dial = Dial { counter: 50 };
 
     for instruction in instructions {
-        match instruction.direction {
+        println!("\nInstruction: {:?}", instruction);
+        _ = match instruction.direction {
             Direction::Left => dial.decrement(instruction.amount),
             Direction::Right => dial.increment(instruction.amount),
-        }
+        };
 
         if dial.counter == 0 {
             count_of_zeros += 1;
         }
+    }
+
+    println!("Result: {}", count_of_zeros);
+}
+
+fn part2() {
+    let input = fs::read_to_string("input.txt").expect("couldn't find part-1.txt");
+    // let input = fs::read_to_string("test.txt").expect("couldn't find part-1.txt");
+    let instructions = input.split_whitespace().map(|item| Instruction::new(item));
+
+    let mut count_of_zeros = 0;
+    let mut dial = Dial { counter: 50 };
+
+    for instruction in instructions {
+        println!("\nInstruction: {:?}", instruction);
+        count_of_zeros += match instruction.direction {
+            Direction::Left => dial.decrement(instruction.amount),
+            Direction::Right => dial.increment(instruction.amount),
+        };
+
+        println!("Count of Zeros: {}", count_of_zeros);
     }
 
     println!("Result: {}", count_of_zeros);
@@ -26,12 +53,52 @@ struct Dial {
 }
 
 impl Dial {
-    fn increment(&mut self, amount: i32) {
-        self.counter = (self.counter + amount).rem_euclid(100);
+    fn increment(&mut self, mut amount: i32) -> i32 {
+        let mut zeros_touched = 0;
+
+        loop {
+            if amount == 0 {
+                break;
+            }
+
+            if self.counter == 99 {
+                self.counter = 0;
+                amount -= 1;
+            } else {
+                self.counter += 1;
+                amount -= 1;
+            }
+
+            if self.counter == 0 {
+                zeros_touched += 1;
+            }
+        }
+
+        zeros_touched
     }
 
-    fn decrement(&mut self, amount: i32) {
-        self.counter = (self.counter - amount).rem_euclid(100);
+    fn decrement(&mut self, mut amount: i32) -> i32 {
+        let mut zeros_touched = 0;
+
+        loop {
+            if amount == 0 {
+                break;
+            }
+
+            if self.counter == 0 {
+                self.counter = 99;
+                amount -= 1;
+            } else {
+                self.counter -= 1;
+                amount -= 1;
+            }
+
+            if self.counter == 0 {
+                zeros_touched += 1;
+            }
+        }
+
+        zeros_touched
     }
 }
 
